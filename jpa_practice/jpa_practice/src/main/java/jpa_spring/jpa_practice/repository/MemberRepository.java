8e2db94f192820332053047b63b90acaa3d6b15f
songdaehyun <109258343+songdaehyun@users.noreply.github.com>
2023-05-04 15:51:32 +0900

    Delete jpa_practice/jpa_practice directory

--- jpa_practice/jpa_practice/src/main/java/jpa_spring/jpa_practice/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package jpa_spring.jpa_practice.repository;
-
-import jpa_spring.jpa_practice.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
